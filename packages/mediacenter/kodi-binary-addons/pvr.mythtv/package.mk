df4f1b5edcfb73dd326d2189a7eb7e4fed3a0c80
Lukas Rusak <lorusak@gmail.com>
2016-04-22 14:57:34 -0700

    kodi-binary-addons: update to latest Jarvis versions

--- packages/mediacenter/kodi-binary-addons/pvr.mythtv/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.mythtv/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.mythtv"
-PKG_VERSION="f6b0483"
+PKG_VERSION="18ee0b5"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
