8cee73ea4a72fa687342ba73319eff700ff54248
kszaq <kszaquitto@gmail.com>
2016-05-08 00:13:49 +0200

    packages/linux: update my Amlogic kernel to bd872fa

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -35,7 +35,7 @@ case "$LINUX" in
     PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
     ;;
   amlogic-kszaq)
-    PKG_VERSION="4e597d35"
+    PKG_VERSION="bd872fa"
     PKG_URL="https://github.com/kszaq/linux/archive/$PKG_VERSION.tar.gz"
     ;;
   imx6)
