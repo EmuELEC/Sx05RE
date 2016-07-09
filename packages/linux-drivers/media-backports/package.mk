7ea49cd0887f2651f9ec596bcabdfa8a6957c0b6
kszaq <kszaquitto@gmail.com>
2016-07-09 23:31:28 +0200

    media-backports: new package

---
+++ packages/linux-drivers/media-backports/package.mk
@@ -0,0 +1,59 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="media-backports"
+PKG_VERSION="4.4.2-1"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://backports.wiki.kernel.org"
+PKG_URL="http://www.kernel.org/pub/linux/kernel/projects/backports/stable/v4.4.2/backports-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_PRIORITY="optional"
+PKG_SECTION="driver"
+PKG_SHORTDESC="Linux media drivers backports"
+PKG_LONGDESC="Linux media drivers backports"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+post_unpack() {
+  mv $BUILD/backports-$PKG_VERSION $ROOT/$PKG_BUILD
+}
+
+make_target() {
+  make defconfig-media \
+       CC=gcc \
+       KLIB=$INSTALL \
+       KLIB_BUILD=$(kernel_path)
+  LDFLAGS="" CFLAGS="" make \
+       KLIB=$INSTALL \
+       KLIB_BUILD=$(kernel_path) \
+       ARCH=$TARGET_KERNEL_ARCH \
+       CROSS_COMPILE=$TARGET_PREFIX
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/lib/modules/$(get_module_dir)/updates/$PKG_NAME
+
+  pushd $ROOT/$PKG_BUILD
+  find compat/ -name \*.ko -exec cp --parents {} $INSTALL/lib/modules/$(get_module_dir)/updates/$PKG_NAME \;
+  find drivers/ -name \*.ko -exec cp --parents {} $INSTALL/lib/modules/$(get_module_dir)/updates/$PKG_NAME \;
+  popd
+}
