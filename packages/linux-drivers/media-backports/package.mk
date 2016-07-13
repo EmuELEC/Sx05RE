3777788ffd053421f9cbcfa68b6c68d66d0f5c12
kszaq <kszaquitto@gmail.com>
2016-07-13 21:20:53 +0200

    media-backports: update package.mk

--- packages/linux-drivers/media-backports/package.mk
+++ packages/linux-drivers/media-backports/package.mk
@@ -1,28 +1,29 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - https://LibreELEC.tv
+#      Copyright (C) 2016 Team LibreELEC
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="media-backports"
-PKG_VERSION="4.4.2-1"
+PKG_VERSION="4.4.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://backports.wiki.kernel.org"
-PKG_URL="http://www.kernel.org/pub/linux/kernel/projects/backports/stable/v4.4.2/backports-$PKG_VERSION.tar.xz"
+PKG_URL="http://www.kernel.org/pub/linux/kernel/projects/backports/stable/v$PKG_VERSION/backports-$PKG_VERSION-$PKG_REV.tar.xz"
+PKG_SOURCE_DIR="backports-$PKG_VERSION-$PKG_REV"
 PKG_DEPENDS_TARGET="toolchain linux"
 PKG_NEED_UNPACK="$LINUX_DEPENDS"
 PKG_PRIORITY="optional"
@@ -32,10 +33,6 @@ PKG_LONGDESC="Linux media drivers backports"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
-
-post_unpack() {
-  mv $BUILD/backports-$PKG_VERSION $ROOT/$PKG_BUILD
-}
 
 make_target() {
   make defconfig-media \
