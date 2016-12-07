576107b936cd7a71c304cdba95f300db012226a2
chewitt <github@chrishewitt.net>
2016-12-07 20:06:52 +0000

    settings: bump to 0.7.5

--- packages/mediacenter/LibreELEC-settings/package.mk
+++ packages/mediacenter/LibreELEC-settings/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="LibreELEC-settings"
-PKG_VERSION="0fa8795"
+PKG_VERSION="08f0750"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
