4f0b2ddd33c7ced68be234bb33e4e252ac844fc8
kszaq <kszaquitto@gmail.com>
2017-01-16 18:32:00 +0100

    gcc-linaro-aarch64-linux-gnu: update to 6.2.1-2016.11

--- packages/lang/gcc-linaro-aarch64-linux-gnu/package.mk
+++ packages/lang/gcc-linaro-aarch64-linux-gnu/package.mk
@@ -17,12 +17,13 @@
 ################################################################################
 
 PKG_NAME="gcc-linaro-aarch64-linux-gnu"
-PKG_VERSION="5.3-2016.02"
+PKG_VERSION="6.2.1-2016.11"
+PKG_VERSION_SHORT="6.2-2016.11"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE=""
-PKG_URL="https://releases.linaro.org/components/toolchain/binaries/${PKG_VERSION}/aarch64-linux-gnu/gcc-linaro-${PKG_VERSION}-x86_64_aarch64-linux-gnu.tar.xz"
+PKG_URL="https://releases.linaro.org/components/toolchain/binaries/${PKG_VERSION_SHORT}/aarch64-linux-gnu/gcc-linaro-${PKG_VERSION}-x86_64_aarch64-linux-gnu.tar.xz"
 PKG_SOURCE_DIR="gcc-linaro-${PKG_VERSION}-x86_64_aarch64-linux-gnu"
 PKG_DEPENDS_HOST="toolchain"
 PKG_PRIORITY="optional"
