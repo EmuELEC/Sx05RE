c7bf19c5c4d75182fa15feba1a2567ee8064fb1d
kszaq <kszaquitto@gmail.com>
2017-01-28 01:42:24 +0100

    media_build: only use project-provided configs for non-Amlogic targets

--- packages/linux-drivers/media_build/package.mk
+++ packages/linux-drivers/media_build/package.mk
@@ -50,7 +50,7 @@ make_target() {
     if [ -f $PKG_DIR/config/generic.config ]; then
       cp $PKG_DIR/config/generic.config v4l/.config
     fi
-  else
+  elif [ "$PROJECT" != "S805" ] && [ "$PROJECT" != "S905" ]; then
     if [ -f $PKG_DIR/config/usb.config ]; then
       cp $PKG_DIR/config/usb.config v4l/.config
     fi
