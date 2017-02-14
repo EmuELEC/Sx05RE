b2307d830b16341bb05ed879f817756136a747e6
kszaq <kszaquitto@gmail.com>
2017-02-14 16:28:55 +0100

    projects/S905: set uenv variables only when updating dtb residing on internal partition

--- projects/S905/filesystem/usr/share/bootloader/update.sh
+++ projects/S905/filesystem/usr/share/bootloader/update.sh
@@ -49,17 +49,17 @@ for arg in $(cat /proc/cmdline); do
 
       if [ -f "$UPDATE_DTB_SOURCE" ] ; then
         echo "Updating device tree from $UPDATE_DTB_SOURCE..."
-        LD_LIBRARY_PATH="$SYSTEM_ROOT/lib" $SYSTEM_ROOT/usr/sbin/fw_setenv upgrade_step 3 2>/dev/null
         case $boot in
           /dev/system)
+            LD_LIBRARY_PATH="$SYSTEM_ROOT/lib" $SYSTEM_ROOT/usr/sbin/fw_setenv upgrade_step 3 2>/dev/null
             dd if=$UPDATE_DTB_SOURCE of="/dev/dtb" bs=256k
+            LD_LIBRARY_PATH="$SYSTEM_ROOT/lib" $SYSTEM_ROOT/usr/sbin/fw_setenv upgrade_step 2 2>/dev/null
             ;;
           /dev/mmc*|LABEL=*)
             mount -o rw,remount $BOOT_ROOT
             cp -f $UPDATE_DTB_SOURCE "$BOOT_ROOT/dtb.img"
             ;;
         esac
-        LD_LIBRARY_PATH="$SYSTEM_ROOT/lib" $SYSTEM_ROOT/usr/sbin/fw_setenv upgrade_step 2 2>/dev/null
       fi
       ;;
     disk=*)
