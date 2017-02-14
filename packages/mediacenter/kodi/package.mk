2ffc1eae15507f295980f5060fe6c07a4efec6e6
kszaq <kszaquitto@gmail.com>
2017-02-14 20:30:41 +0100

    kodi: update to b0bc68b

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="b978678"
+PKG_VERSION="b0bc68b"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
