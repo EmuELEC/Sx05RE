7736847aa7e26a24ff14f14e120a553b62198cfa
kszaq <kszaquitto@gmail.com>
2017-02-20 01:35:50 +0100

    arm-mem: also use in S805 and S905 projects

--- packages/virtual/libc/package.mk
+++ packages/virtual/libc/package.mk
@@ -33,7 +33,9 @@ PKG_LONGDESC="Meta package for installing various tools and libs needed for libc
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-if [ "$BOOTLOADER" = "bcm2835-bootloader" ]; then
+if [ "$BOOTLOADER" = "bcm2835-bootloader" ] \
+  || [ "$PROJECT" = "S805" ] \
+  || [ "$PROJECT" = "S905" -a "$ARCH" = "arm" ] ; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET arm-mem"
   PKG_DEPENDS_INIT="$PKG_DEPENDS_INIT arm-mem:init"
 fi
