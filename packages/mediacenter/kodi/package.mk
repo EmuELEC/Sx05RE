859313481bf90e856f3e770d12fc3353e679b9ee
kszaq <kszaquitto@gmail.com>
2017-02-21 16:43:14 +0100

    kodi: update to ff85138

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="b0bc68b"
+PKG_VERSION="ff85138"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
