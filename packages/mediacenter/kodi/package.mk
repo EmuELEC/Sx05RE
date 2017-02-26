b684c4510b50e4946f03f7ac7f0c618c86a0e53b
kszaq <kszaquitto@gmail.com>
2017-02-26 12:35:28 +0100

    kodi: update to 07ae746

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="82eaae9"
+PKG_VERSION="07ae746"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
