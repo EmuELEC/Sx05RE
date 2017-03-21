97110b678659f3a59337fa31aa01600aad91ca0e
Radostan Riedel <raybuntu@googlemail.com>
2017-03-21 15:15:12 +0100

    tvheadend42: update to 1fddf32

--- packages/addons/service/tvheadend42/package.mk
+++ packages/addons/service/tvheadend42/package.mk
@@ -17,9 +17,9 @@
 ################################################################################
 
 PKG_NAME="tvheadend42"
-PKG_VERSION="e5f5a42"
-PKG_VERSION_NUMBER="4.1.2415"
-PKG_REV="109"
+PKG_VERSION="1fddf32"
+PKG_VERSION_NUMBER="4.1.2484"
+PKG_REV="110"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
