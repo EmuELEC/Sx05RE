d8e2f9b4277e6d79004adb6f2fa19824b74ab6f1
shantigilbert <shantic@gmail.com>
2017-05-28 12:03:27 -0500

    Qt-everywhere still doesn't compile

--- packages/sx05re/experimental/pegasus-frontend/qt-everywhere/package.mk
+++ packages/sx05re/experimental/pegasus-frontend/qt-everywhere/package.mk
@@ -1,79 +1,51 @@
-################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2009-2016 Lukas Rusak (lrusak@libreelec.tv)
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
-
-PKG_NAME="qt5"
+PKG_NAME="qt-everywhere"
 PKG_VERSION="5.8.0"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://qt-project.org"
-PKG_URL="http://download.qt.io/official_releases/qt/5.8/$PKG_VERSION/single/qt-everywhere-opensource-src-$PKG_VERSION.tar.xz"
-#http://download.qt.io/official_releases/qt/5.8/5.8.0/single/qt-everywhere-opensource-src-5.8.0.tar.xz
-PKG_DEPENDS_TARGET="pcre zlib sqlite"
-PKG_SOURCE_DIR="qt-everywhere-opensource-src-$PKG_VERSION"
-PKG_SHORTDESC="A cross-platform application and UI framework"
+PKG_URL="http://download.qt.io/official_releases/qt/5.8/$PKG_VERSION/single/$PKG_NAME-opensource-src-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="pcre zlib"
+PKG_SOURCE_DIR="$PKG_NAME-opensource-src-$PKG_VERSION"
 PKG_LONGDESC="A cross-platform application and UI framework"
-PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="-prefix /usr
                            -sysroot $SYSROOT_PREFIX
                            -hostprefix $ROOT/$TOOLCHAIN
                            -device linux-libreelec-g++
-                           -opensource 
-                           -static
-		           -confirm-license
+                           -opensource -confirm-license
                            -release
+                           -static
                            -make libs
                            -force-pkg-config
+                           -no-accessibility
                            -no-sql-sqlite
                            -no-sql-mysql
+                           -no-qml-debug
                            -system-zlib
+                           -no-mtdev
                            -no-gif
-                           -no-libpng
                            -no-libjpeg
                            -no-harfbuzz
                            -no-openssl
                            -no-libproxy
                            -system-pcre
                            -no-glib
+                           -no-pulseaudio
+                           -no-alsa
                            -silent
                            -no-cups
                            -no-iconv
                            -no-evdev
                            -no-tslib
                            -no-icu
-                           -strip
+                           -no-strip
                            -no-fontconfig
+                           -no-dbus
                            -no-opengl
                            -no-libudev
                            -no-libinput
-                           -no-eglfs
-                           -skip qtwebengine
-                           -skip wayland
-			   -no-rpath
-			   -opengl es2
-			   -fontconfig \
-	-no-xcb 
-	-system-zlib \
-	-system-libpng \
-	-system-libjpeg \
-	-verbose
--skip qtxmlpatterns"
-
+                           -no-gstreamer
+                           -no-eglfs"
 
 configure_target() {
   QMAKE_CONF_DIR="qtbase/mkspecs/devices/linux-libreelec-g++"
@@ -105,3 +77,4 @@ configure_target() {
   unset CC CXX LD RANLIB AR AS CPPFLAGS CFLAGS LDFLAGS CXXFLAGS
   ./configure $PKG_CONFIGURE_OPTS_TARGET
 }
+
