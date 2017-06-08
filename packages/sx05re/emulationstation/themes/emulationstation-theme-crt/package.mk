620b6154eb3ba768129aca44a25abba1700baf53
shantigilbert <shantic@gmail.com>
2017-06-08 18:52:18 -0500

    bump ES theme CRT

--- packages/sx05re/emulationstation/themes/emulationstation-theme-crt/package.mk
+++ packages/sx05re/emulationstation/themes/emulationstation-theme-crt/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="emulationstation-theme-crt"
-PKG_VERSION="a427bfd"
+PKG_VERSION="b92042a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
