942638d33f53b77c79e547ea3462215b7f78d18b
shantigilbert <shantic@gmail.com>
2017-06-09 14:51:53 -0500

    bump reicast

--- packages/sx05re/reicastsa/package.mk
+++ packages/sx05re/reicastsa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="reicastsa"
-PKG_VERSION="ca837e1"
+PKG_VERSION="3c802cd"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/reicast/reicast-emulator"
