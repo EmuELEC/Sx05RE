6ba513521ea99531722931a0bc01ab6f3693ece7
shantigilbert <shantic@gmail.com>
2017-06-08 19:35:03 -0500

    Fix typo on pulseaudio package
    not sure if this does anything.

--- packages/audio/pulseaudio/package.mk
+++ packages/audio/pulseaudio/package.mk
@@ -82,7 +82,7 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-silent-rules \
                            --disable-bluez5-ofono-headset \
                            --disable-bluez5-native-headset \
                            --enable-udev \
-                           --with-udev-rules-dir=/usr/lib/udev/rules.d
+                           --with-udev-rules-dir=/usr/lib/udev/rules.d \
                            --disable-hal-compat \
                            --enable-ipv6 \
                            --enable-openssl \
