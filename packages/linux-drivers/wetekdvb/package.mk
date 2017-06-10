a90ff63d0e36bae27dceb3d03a7898a280694c19
Alex Deryskyba <alex@codesnake.com>
2017-06-10 15:42:28 +0200

    wetekdvb: update WeTek proprietary DVB modules to wetekdvb-20170608
    * fix a crash when the driver is loading on WeTek Play 1 with DVB-T2 tuner or without a tuner
    * make driver compatible with media_build

--- packages/linux-drivers/wetekdvb/package.mk
+++ packages/linux-drivers/wetekdvb/package.mk
@@ -17,14 +17,7 @@
 ################################################################################
 
 PKG_NAME="wetekdvb"
-case "$PROJECT" in
-  WeTek_Play)
-    PKG_VERSION="20170116"
-    ;;
-  *)
-    PKG_VERSION="20170404"
-    ;;
-esac
+PKG_VERSION="20170608"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.wetek.com/"
