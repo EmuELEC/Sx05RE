6f935921668d110fd57b9e58c07a4cbc0b66ec5b
Unknown <shantic@gmail.com>
2017-06-11 21:32:03 -0500

    Removed USB rom script
    It's really not needed for Retroarch

--- packages/sx05re/sx05re/autostart.sh
+++ packages/sx05re/sx05re/autostart.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
+
+#this is not needed if we don't use ES
+
 # USB roms 
-sleep 2
+#sleep 2
 
 #name of the file we need to put in the roms folder in your USB or SDCARD 
-ROMFILE="sx05reroms"
+#ROMFILE="sx05reroms"
 
 
 # we look for the file in the rompath
-FULLPATHTOROMS="$(find /media/*/roms/ -name $ROMFILE -maxdepth 1)"
+#FULLPATHTOROMS="$(find /media/*/roms/ -name $ROMFILE -maxdepth 1)"
 #echo "${FULLPATHTOROMS}"
 
 
-if [[ -z "${FULLPATHTOROMS}" ]]; then
+#if [[ -z "${FULLPATHTOROMS}" ]]; then
 # echo "can't find roms"
 
-    if [ -L /storage/roms ]; then
-      rm /storage/roms
-      mv /storage/roms2 /storage/roms
- fi
-    else
-      mv /storage/roms /storage/roms2
+#    if [ -L /storage/roms ]; then
+#      rm /storage/roms
+#      mv /storage/roms2 /storage/roms
+# fi
+#    else
+#      mv /storage/roms /storage/roms2
       #echo "move the roms folder"
  
        # we strip the name of the file.
-       PATHTOROMS=${FULLPATHTOROMS%$ROMFILE}
+#       PATHTOROMS=${FULLPATHTOROMS%$ROMFILE}
 
        #we create the symlink to the roms in our USB
-       ln -sf $PATHTOROMS /storage/roms
+#       ln -sf $PATHTOROMS /storage/roms
 
- fi
+# fi
 
 
 #hacky way to get samba on boot
