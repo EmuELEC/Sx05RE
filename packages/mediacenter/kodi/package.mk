ad18fd5bba6dd996289521607368ff93e7623f90
kszaq <kszaquitto@gmail.com>
2017-07-04 00:45:42 +0200

    kodi: bump to a261e06

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="4fe4889"
+PKG_VERSION="a261e06"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
