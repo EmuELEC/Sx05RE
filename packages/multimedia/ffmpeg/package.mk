d7ee25e9a6de53a1d015a2373f657befbe44def8
kszaq <kszaquitto@gmail.com>
2017-07-07 21:02:09 +0200

    ffmpeg: don't use hardcoded tables for arm, compile with thumb

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -61,10 +61,7 @@ fi
 
 case "$TARGET_ARCH" in
   arm)
-    FFMPEG_TABLES="--enable-hardcoded-tables"
-    ;;
-  *)
-    FFMPEG_TABLES="--disable-hardcoded-tables"
+    CFLAGS="$CFLAGS -mthumb"
     ;;
 esac
 
@@ -158,7 +155,6 @@ configure_target() {
               $FFMPEG_VDPAU \
               --disable-dxva2 \
               --enable-runtime-cpudetect \
-              $FFMPEG_TABLES \
               --disable-memalign-hack \
               --disable-encoders \
               --enable-encoder=ac3 \
