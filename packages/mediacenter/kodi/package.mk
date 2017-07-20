d78c0009ac71f2dd7f37af695f107cbb8dae9f7c
kszaq <kszaquitto@gmail.com>
2017-07-20 23:13:14 +0200

    kodi: bump to b22184d

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="0362b76"
+PKG_VERSION="b22184d"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
