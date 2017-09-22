b0324e36db3f2dbfc29409cb0d56ff4712ac01a0
Unknown <shantic@gmail.com>
2017-09-22 14:22:26 -0500

    bump Pesgasus FE

--- packages/sx05re/experimental/pegasus-frontend/package.mk
+++ packages/sx05re/experimental/pegasus-frontend/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="pegasus-frontend"
-PKG_VERSION="14185c7"
+PKG_VERSION="daba453"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
