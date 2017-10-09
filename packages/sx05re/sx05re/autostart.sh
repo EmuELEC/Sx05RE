85eeb4dccc3f9a22378c12a6a54be5f5bb79249d
数码视讯Q5 <SumavisionQ5@users.noreply.github.com>
2017-10-09 18:24:05 +0800

    Update autostart.sh

--- packages/sx05re/sx05re/autostart.sh
+++ packages/sx05re/sx05re/autostart.sh
@@ -2,6 +2,75 @@
 # USB roms 
 sleep 2
 
+# Clean cache garbage when boot up.
+rm -rf /storage/.cache/cores/*
+
+# Read the video output mode and set it for sx05re to avoid video flicking.
+MODE=`cat /sys/class/display/mode`;
+if [ "$MODE" = "480p60hz" ]; then
+fbset -fb /dev/fb0 -g 720 480 720 960 32
+fi
+
+MODE=`cat /sys/class/display/mode`;
+if [ "$MODE" = "576p50hz" ]; then
+fbset -fb /dev/fb0 -g 720 576 720 1152 32
+fi
+
+MODE=`cat /sys/class/display/mode`;
+if [ "$MODE" = "720p60hz" ]; then
+fbset -fb /dev/fb0 -g 1280 720 1280 1440 32
+fi
+
+MODE=`cat /sys/class/display/mode`;
+if [ "$MODE" = "720p50hz" ]; then
+fbset -fb /dev/fb0 -g 1280 720 1280 1440 32
+fi
+
+MODE=`cat /sys/class/display/mode`;
+if [ "$MODE" = "1080p60hz" ]; then
+fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
+fi
+
+MODE=`cat /sys/class/display/mode`;
+if [ "$MODE" = "1080i60hz" ]; then
+fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
+fi
+
+MODE=`cat /sys/class/display/mode`;
+if [ "$MODE" = "1080i50hz" ]; then
+fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
+fi
+
+MODE=`cat /sys/class/display/mode`;
+if [ "$MODE" = "1080p50hz" ]; then
+fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
+fi
+
+MODE=`cat /sys/class/display/mode`;
+if [ "$MODE" = "480cvbs" ]; then
+fbset -fb /dev/fb0 -g 1280 960 1280 1920 32
+fbset -fb /dev/fb1 -g 32 32 32 32 32
+echo 0 0 1279 959 > /sys/class/graphics/fb0/free_scale_axis
+echo 30 10 669 469 > /sys/class/graphics/fb0/window_axis
+echo 640 > /sys/class/graphics/fb0/scale_width
+echo 480 > /sys/class/graphics/fb0/scale_height
+echo 0x10001 > /sys/class/graphics/fb0/free_scale
+fi
+
+MODE=`cat /sys/class/display/mode`;
+if [ "$MODE" = "576cvbs" ]; then
+fbset -fb /dev/fb0 -g 1280 960 1280 1920 32
+fbset -fb /dev/fb1 -g 32 32 32 32 32
+echo 0 0 1279 959 > /sys/class/graphics/fb0/free_scale_axis
+echo 35 20 680 565 > /sys/class/graphics/fb0/window_axis
+echo 720 > /sys/class/graphics/fb0/scale_width
+echo 576 > /sys/class/graphics/fb0/scale_height
+echo 0x10001 > /sys/class/graphics/fb0/free_scale
+fi
+# End of reading the video output mode and setting it for sx05re to avoid video flicking.
+# The codes can be simplified with "elseif" sentences.
+# The codes for 480I and 576I are adjusted to avoid overscan.
+
 #since the ROMS folder is not getting created at boot, we check if it exists, if it doesn't then we create it. 
 if [ ! -d "/storage/roms" ] || [ ! -d "/storage/roms2" ]; then
   mkdir /storage/roms
