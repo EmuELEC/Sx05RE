08f6ca7174047440da6232229224375e46355fc1
Unknown <shantic@gmail.com>
2018-02-07 22:25:04 -0600

    Bump EmulationStation

--- packages/sx05re/emulationstation/package.mk
+++ packages/sx05re/emulationstation/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="emulationstation"
-PKG_VERSION="5e9c296"
+PKG_VERSION="3adadc9"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
