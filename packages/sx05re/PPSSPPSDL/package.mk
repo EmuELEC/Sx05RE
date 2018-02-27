62af1343a69ea5c18399deebb1e6718196c77f1b
Unknown <shantic@gmail.com>
2018-02-26 20:52:30 -0600

    Bump PPSSPPSDL
    and cleanup package

--- packages/sx05re/PPSSPPSDL/package.mk
+++ packages/sx05re/PPSSPPSDL/package.mk
@@ -17,60 +17,36 @@
 ################################################################################
 
 PKG_NAME="PPSSPPSDL"
-PKG_VERSION="5eba689"
+PKG_VERSION="9a732fe"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
-PKG_SITE="https://github.com/hrydgard/ppsspp.git"
-PKG_URL="https://github.com/hrydgard/"
+PKG_SITE="https://github.com/hrydgard/ppsspp"
+PKG_GIT_URL="$PKG_SITE"
 PKG_DEPENDS_TARGET="toolchain ffmpeg libzip libpng SDL2 zlib zip linux"
 PKG_SHORTDESC="PPSSPPDL"
 PKG_LONGDESC="PPSSPP Standalone"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-if [ "$TARGET_ARCH" == "aarch64" ]; then
-
-PKG_CMAKE_OPTS_TARGET="-DUSING_FBDEV=1  \
-                       -DUSING_GLES2=1  \
-                       -DUSE_FFMPEG=1  \
-                       -DARM64=1  \
-		       -DSHARED_LIBZIP=1 \
-                       -DARM_NEON=1"
-else
-PKG_CMAKE_OPTS_TARGET="-DUSING_FBDEV=1  \
-                       -DUSING_GLES2=1  \
-                       -DUSE_FFMPEG=1  \
-                       -DARMV7=1  \
-		       -DSHARED_LIBZIP=1 \
-                       -DARM_NEON=1"
-fi
+PKG_CMAKE_OPTS_TARGET="-DUSING_FBDEV=1 -DUSING_GLES2=1 -DUSE_FFMPEG=1 -DARMV7=1 -DSHARED_LIBZIP=1 -DARM_NEON=1"
+#-DNO_VULKAN"
 
 unpack() { 
  cp $PKG_DIR/zipconf.h $SYSROOT_PREFIX/usr/include/zipconf.h 
- git clone --recursive https://github.com/hrydgard/ppsspp.git $PKG_BUILD
-  cd $PKG_BUILD
-  git reset --hard $PKG_VERSION
-  rm -rf .git
-  cd $ROOT
- 
 }
-
 
 pre_configure_target() {
 strip_lto
 }
 
 make_target() {
-
 make
 }
-
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/bin
     cp $PKG_DIR/ppsspp.sh $INSTALL/usr/bin/ppsspp.sh
     cp `find . -name "PPSSPPSDL" | xargs echo` $INSTALL/usr/bin/PPSSPPSDL
- #mkdir -p $INSTALL/usr/bin/
     cp -r `find . -name "assets" | xargs echo` $INSTALL/usr/bin/
 } 
