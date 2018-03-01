405a7ecc3b95b98d54a43a61e97db652466fd980
Unknown <shantic@gmail.com>
2018-02-28 20:22:28 -0600

    Bump Advancemame

--- packages/sx05re/advancemame/package.mk
+++ packages/sx05re/advancemame/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="advancemame"
-PKG_VERSION="480ad7c"
+PKG_VERSION="7e2a569"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MAME"
