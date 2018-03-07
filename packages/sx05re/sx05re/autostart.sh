b6010e883f1044c6dc8ab19654ba3d9b9ff69892
Unknown <shantic@gmail.com>
2018-03-07 01:42:32 -0600

    autostart.sh cleanup

--- packages/sx05re/sx05re/autostart.sh
+++ packages/sx05re/sx05re/autostart.sh
@@ -7,69 +7,55 @@ rm -rf /storage/.cache/cores/*
 
 # Read the video output mode and set it for sx05re to avoid video flicking.
 MODE=`cat /sys/class/display/mode`;
-if [ "$MODE" = "480p60hz" ]; then
-fbset -fb /dev/fb0 -g 720 480 720 960 32
-fi
 
-MODE=`cat /sys/class/display/mode`;
-if [ "$MODE" = "576p50hz" ]; then
-fbset -fb /dev/fb0 -g 720 576 720 1152 32
-fi
+case "$MODE" in
+"480p60hz")
+	fbset -fb /dev/fb0 -g 720 480 720 960 32
+	;;
+"576p50hz")
+	fbset -fb /dev/fb0 -g 720 576 720 1152 32
+	;;
+"720p60hz")
+	fbset -fb /dev/fb0 -g 1280 720 1280 1440 32
+	;;
+"720p50hz")
+	echo 720p60hz > /sys/class/display/mode
+	fbset -fb /dev/fb0 -g 1280 720 1280 1440 32
+	;;
+"1080p60hz")
+	fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
+	;;
+"1080i60hz")
+	fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
+	;;
+"1080i50hz")
+	echo 1080i60hz > /sys/class/display/mode
+	fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
+	;;
+"1080p50hz")
+	echo 1080p60hz > /sys/class/display/mode
+	fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
+	;;
+"480cvbs")
+	fbset -fb /dev/fb0 -g 1280 960 1280 1920 32
+	fbset -fb /dev/fb1 -g 32 32 32 32 32
+	echo 0 0 1279 959 > /sys/class/graphics/fb0/free_scale_axis
+	echo 30 10 669 469 > /sys/class/graphics/fb0/window_axis
+	echo 640 > /sys/class/graphics/fb0/scale_width
+	echo 480 > /sys/class/graphics/fb0/scale_height
+	echo 0x10001 > /sys/class/graphics/fb0/free_scale
+	;;
+"576cvbs")
+	fbset -fb /dev/fb0 -g 1280 960 1280 1920 32
+	fbset -fb /dev/fb1 -g 32 32 32 32 32
+	echo 0 0 1279 959 > /sys/class/graphics/fb0/free_scale_axis
+	echo 35 20 680 565 > /sys/class/graphics/fb0/window_axis
+	echo 720 > /sys/class/graphics/fb0/scale_width
+	echo 576 > /sys/class/graphics/fb0/scale_height
+	echo 0x10001 > /sys/class/graphics/fb0/free_scale
+	;;
+esac
 
-MODE=`cat /sys/class/display/mode`;
-if [ "$MODE" = "720p60hz" ]; then
-fbset -fb /dev/fb0 -g 1280 720 1280 1440 32
-fi
-
-MODE=`cat /sys/class/display/mode`;
-if [ "$MODE" = "720p50hz" ]; then
-echo 720p60hz > /sys/class/display/mode
-fbset -fb /dev/fb0 -g 1280 720 1280 1440 32
-fi
-
-MODE=`cat /sys/class/display/mode`;
-if [ "$MODE" = "1080p60hz" ]; then
-fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
-fi
-
-MODE=`cat /sys/class/display/mode`;
-if [ "$MODE" = "1080i60hz" ]; then
-fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
-fi
-
-MODE=`cat /sys/class/display/mode`;
-if [ "$MODE" = "1080i50hz" ]; then
-echo 1080i60hz > /sys/class/display/mode
-fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
-fi
-
-MODE=`cat /sys/class/display/mode`;
-if [ "$MODE" = "1080p50hz" ]; then
-echo 1080p60hz > /sys/class/display/mode
-fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
-fi
-
-MODE=`cat /sys/class/display/mode`;
-if [ "$MODE" = "480cvbs" ]; then
-fbset -fb /dev/fb0 -g 1280 960 1280 1920 32
-fbset -fb /dev/fb1 -g 32 32 32 32 32
-echo 0 0 1279 959 > /sys/class/graphics/fb0/free_scale_axis
-echo 30 10 669 469 > /sys/class/graphics/fb0/window_axis
-echo 640 > /sys/class/graphics/fb0/scale_width
-echo 480 > /sys/class/graphics/fb0/scale_height
-echo 0x10001 > /sys/class/graphics/fb0/free_scale
-fi
-
-MODE=`cat /sys/class/display/mode`;
-if [ "$MODE" = "576cvbs" ]; then
-fbset -fb /dev/fb0 -g 1280 960 1280 1920 32
-fbset -fb /dev/fb1 -g 32 32 32 32 32
-echo 0 0 1279 959 > /sys/class/graphics/fb0/free_scale_axis
-echo 35 20 680 565 > /sys/class/graphics/fb0/window_axis
-echo 720 > /sys/class/graphics/fb0/scale_width
-echo 576 > /sys/class/graphics/fb0/scale_height
-echo 0x10001 > /sys/class/graphics/fb0/free_scale
-fi
 # End of reading the video output mode and setting it for sx05re to avoid video flicking.
 # The codes can be simplified with "elseif" sentences.
 # The codes for 480I and 576I are adjusted to avoid overscan.
@@ -108,7 +94,6 @@ if [[ -z "${FULLPATHTOROMS}" ]]; then
 
  fi
 
-
 #set reicast BIOS dir to point to /storage/roms/bios
 if [ ! -L /storage/.local/share/reicast/data ]; then
 mkdir -p /storage/.local/share/reicast 
@@ -116,28 +101,14 @@ rm -rf /storage/.local/share/reicast/data
 ln -s /storage/roms/bios /storage/.local/share/reicast/data
 fi
 
+
 #hacky way to get samba on boot
 /usr/lib/samba/samba-config
 systemctl start smbd
 
-# THESE CHANGES ARE UNSTESTED! I DONT KNOW IF YOUR DEVICE WILL EXPLODE UNCOMMENT UNDER YOUR OWN RISK!
-
-# Set the device to run at top speed all the time, it has made a lot of emulators run better when all of them are set.
-# Make sure you have cooling other than the default.
-
-# Echoing 5 will get you 792MHz for S905 and 742MHz for S905X
-#echo 5 > /sys/class/mpgpu/cur_freq
-#echo 5 > /sys/class/mpgpu/max_freq
-#echo 5 > /sys/class/mpgpu/min_freq
-
-##set CPU max
-#echo 1536000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-#echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-
 #make sure the requirement to run kodi is met.
 touch  /var/lock/start.kodi
 
- #if you don't want EmulationStation to start first, comment the next 3 lines
+ #if you don't want the emulator front end to start first, comment the next 3 lines
  rm -rf /var/lock/start.kodi
- touch /var/lock/start.games
- systemctl start emustation
+ /usr/bin/startfe.sh &
