dcb83a425d8634dc813a487ce24f6b9f90bcf3ed
Unknown <shantic@gmail.com>
2018-04-05 09:46:29 -0500

    Move ES scripts to the frontend package

--- packages/sx05re/emulationstation/package.mk
+++ packages/sx05re/emulationstation/package.mk
@@ -44,16 +44,10 @@ mkdir -p $PKG_BUILD/external/pugixml
 }
 
 post_makeinstall_target() {
-  mkdir -p $INSTALL/usr/bin
-    cp $PKG_DIR/scripts/* $INSTALL/usr/bin
-    chmod +x $INSTALL/usr/bin/*
 
   mkdir -p $INSTALL/usr/bin/resources
     cp -rf $PKG_BUILD/resources/* $INSTALL/usr/bin/resources/
 
-  mkdir -p $INSTALL/usr/config/emulationstation
-    cp $PKG_DIR/config/* $INSTALL/usr/config/emulationstation
-  
    mkdir -p $INSTALL/etc/emulationstation/
    ln -sf /storage/.config/emulationstation/themes $INSTALL/etc/emulationstation/
 }
