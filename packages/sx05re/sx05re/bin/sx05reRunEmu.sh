7133f2886d7d5fe8f2296b0500799ae212ed0ff9
Unknown <shantic@gmail.com>
2018-04-06 15:07:52 -0500

    Attempt to add log to Sx05reRunEmu.sh
    It doesn't quite work yet, but its a start

--- packages/sx05re/sx05re/bin/sx05reRunEmu.sh
+++ packages/sx05re/sx05re/bin/sx05reRunEmu.sh
@@ -2,7 +2,7 @@
 
 hdmimode=`cat /sys/class/display/mode`;
 
-# Set framebuffer geometry to match the resolution 
+# Set framebuffer geometry to match the resolution, splash should change according to the resolution. 
 case $hdmimode in
   480*)            X=720  Y=480  SPLASH="/storage/.config/splash/splash-1080.png" ;;
   576*)            X=720  Y=576  SPLASH="/storage/.config/splash/splash-1080.png" ;;
@@ -10,40 +10,50 @@ case $hdmimode in
   *)               X=1920 Y=1080 SPLASH="/storage/.config/splash/splash-1080.png" ;;
 esac
 
-# Most Emulators work best at 16bpp, Retroarch shaders work best and PPSSPPSDL ONLY runs at 16bpp
-fbset -fb /dev/fb0 -g  $X $Y 1920 2160 16
+# Set BPP to 16,Most RetroArch shaders work best and PPSSPPSDL ONLY runs at 16bpp, we might need to set this in autorun.sh and set it to 32bpp when we run Kodi as it is now it generates an annoying flicker when the switch.
+BPP="16"
 
-# splash screen, not sure if this is the best way to do it, but it works so far, but not as good as I want it too with PPSSPPSDL and advmame :(  
+# Set the variables
+CFG="/storage/.emulationstation/es_settings.cfg"
+SX05RELOG="/storage/sx05re.log"
+PAT="s|\s*<string name=\"Sx05RE_$1_CORE\" value=\"\(.*\)\" />|\1|p"
+EMU=$(sed -n "$PAT" "$CFG")
+
+# Clear the log file
+echo "Sx05RE Run Log" > $SX05RELOG
+
+# Default run command
+RUNTHIS=`/usr/bin/retroarch -L /tmp/cores/${EMU}_libretro.so "$2"`
+
+# Read the first argument to see if its MAME or PSP
+case $1 in
+"MAME")
+      if [ "$EMU" = "AdvanceMame" ]; then
+   # advmame runs best at 32bpp   
+   BPP="32" 
+   RUNTHIS=`/usr/bin/advmame.sh "$2"`
+   fi
+       ;;
+"PSP")
+      if [ "$EMU" = "PPSSPPSA" ]; then
+   RUNTHIS=`/usr/bin/ppsspp.sh "$2"`
+      fi
+        ;;
+esac
+
+fbset -fb /dev/fb0 -g  $X $Y 1920 2160 $BPP
+
+# Splash screen, not sure if this is the best way to do it, but it works so far, but not as good as I want it too with PPSSPPSDL and advmame :(
 (
   fbi $SPLASH -noverbose > /dev/null 2>&1
 )&
 
-CFG="/storage/.emulationstation/es_settings.cfg"
+# Write the command to the log file, fomr reason it does not work :/
+echo "Run Command is:" >> $SX05RELOG 
+echo $RUNTHIS >> $SX05RELOG 
 
-PAT="s|\s*<string name=\"Sx05RE_$1_CORE\" value=\"\(.*\)\" />|\1|p"
-EMU=$(sed -n "$PAT" "$CFG")
-
-case $1 in
-"MAME")
-        if [ "$EMU" = "AdvanceMame" ]; then
-   # advmame runs best at 32bpp     
-   fbset -fb /dev/fb0 -g  $X $Y 1920 2160 32
-   /usr/bin/advmame.sh "$2"
-          else
-   /usr/bin/retroarch -L /tmp/cores/${EMU}_libretro.so "$2"
-        fi
-        ;;
-"PSP")
-        if [ "$EMU" = "PPSSPPSA" ]; then
-   /usr/bin/ppsspp.sh "$2"
-          else
-   /usr/bin/retroarch -L /tmp/cores/${EMU}_libretro.so "$2"
-        fi
-        ;;
-*)
-   /usr/bin/retroarch -L /tmp/cores/${EMU}_libretro.so "$2"
-        ;;
-esac
+# Exceute the command and try to output the results to the log file, but no loggin ocurs :/
+eval "$RUNTHIS" >> $SX05RELOG
 
 # Return to 32bit mode
 fbset -fb /dev/fb0 -g  $X $Y 1920 2160 32
