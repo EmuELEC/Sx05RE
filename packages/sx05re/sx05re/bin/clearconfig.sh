e171e72270ab31cc6d6c63ca62ae585ba45e6057
Unknown <shantic@gmail.com>
2018-04-17 15:08:46 -0500

    Added a way to reset Emus, Kodi and System defaults
    Make updating easier if you run into problems.

--- packages/sx05re/sx05re/bin/clearconfig.sh
+++ packages/sx05re/sx05re/bin/clearconfig.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+case $1 in
+"KODI")
+rm -rf /storage/.kodi*
+systemctl reboot
+  ;;
+"EMUS")
+rm -rf /storage/.emulationstation*
+rm -rf /storage/.local*
+rm -rf /storage/.config/emulationstation*
+rm -rf /storage/.config/ppsspp*
+rm -rf /storage/.config/reicast*
+rm -rf /storage/.config/retroarch*
+rm -rf /storage/.advance*
+systemctl reboot
+  ;;
+"ALL")
 rm -rf /storage/.*
 systemctl reboot
-
-
-
+  ;;
+esac
