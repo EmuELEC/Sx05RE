096f33c359924e440c68d6469cf547253c2c8f45
Unknown <shantic@gmail.com>
2018-04-19 10:21:12 -0500

    Set bpp at 16 as default
    Switch to 32 when running Kodi

--- packages/sx05re/sx05re/autostart.sh
+++ packages/sx05re/sx05re/autostart.sh
@@ -5,56 +5,11 @@ sleep 2
 # Clean cache garbage when boot up.
 rm -rf /storage/.cache/cores/*
 
-# Read the video output mode and set it for sx05re to avoid video flicking.
-MODE=`cat /sys/class/display/mode`;
-
-case "$MODE" in
-"480p60hz")
-	fbset -fb /dev/fb0 -g 720 480 720 960 32
-	;;
-"576p50hz")
-	fbset -fb /dev/fb0 -g 720 576 720 1152 32
-	;;
-"720p60hz")
-	fbset -fb /dev/fb0 -g 1280 720 1280 1440 32
-	;;
-"720p50hz")
-	echo 720p60hz > /sys/class/display/mode
-	fbset -fb /dev/fb0 -g 1280 720 1280 1440 32
-	;;
-"1080p60hz")
-	fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
-	;;
-"1080i60hz")
-	fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
-	;;
-"1080i50hz")
-	echo 1080i60hz > /sys/class/display/mode
-	fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
-	;;
-"1080p50hz")
-	echo 1080p60hz > /sys/class/display/mode
-	fbset -fb /dev/fb0 -g 1920 1080 1920 2160 32
-	;;
-"480cvbs")
-	fbset -fb /dev/fb0 -g 1280 960 1280 1920 32
-	fbset -fb /dev/fb1 -g 32 32 32 32 32
-	echo 0 0 1279 959 > /sys/class/graphics/fb0/free_scale_axis
-	echo 30 10 669 469 > /sys/class/graphics/fb0/window_axis
-	echo 640 > /sys/class/graphics/fb0/scale_width
-	echo 480 > /sys/class/graphics/fb0/scale_height
-	echo 0x10001 > /sys/class/graphics/fb0/free_scale
-	;;
-"576cvbs")
-	fbset -fb /dev/fb0 -g 1280 960 1280 1920 32
-	fbset -fb /dev/fb1 -g 32 32 32 32 32
-	echo 0 0 1279 959 > /sys/class/graphics/fb0/free_scale_axis
-	echo 35 20 680 565 > /sys/class/graphics/fb0/window_axis
-	echo 720 > /sys/class/graphics/fb0/scale_width
-	echo 576 > /sys/class/graphics/fb0/scale_height
-	echo 0x10001 > /sys/class/graphics/fb0/free_scale
-	;;
-esac
+/usr/bin/setres.sh
+SPLASH="/storage/.config/splash/splash-1080.png"
+(
+  fbi $SPLASH -noverbose > /dev/null 2>&1
+)&
 
 # End of reading the video output mode and setting it for sx05re to avoid video flicking.
 # The codes can be simplified with "elseif" sentences.
